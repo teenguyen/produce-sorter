9fa717635898e00da13fa97d55a5e0059c6af866
ryuuseiistar <tee.nguyen@live.com.au>
2018-07-22 13:13:22 +1000

    neaaaarly there with the algo

--- src/pages/SortPicker.jsx
+++ src/pages/SortPicker.jsx
@@ -22,27 +22,74 @@ export default class SortPicker extends Component {
             sortedGirls: sortedGirls,
             noneGirls: [],
             pairCount: 0,
-            progress: 50,
+            progress: 0,
             girl1: girl1,
             girl2: girl2,
+            nextGirl: 2,
             left: 0,
-            right: 0
+            right: sortedGirls.length
         }
 
         this.onClick = this.onClick.bind(this);
         this.getIndex = this.getIndex.bind(this);
-        this.insertGirl = this.insertGirl.bind(this);
+        this.getMidGirl = this.getMidGirl.bind(this);
     }
 
     onClick(opt) {
-        let girls = this.state.sortedGirls;
-        const left = 0;
-        const right = this.state.sortedGirls.length;
-        const g1 = this.getIndex(this.state.girl1);
+        let pairCount = this.state.pairCount;
+        let girls = this.state.sortedGirls.slice(); // y u not immutable omfg
+        let left = this.state.left;
+        let right = this.state.right;
+        let g1 = this.getIndex(this.state.girl1);
+
         switch(opt) {
             case GIRL1:
+                // right = g1;
+                if (right - left === 1 || right - left === 0) {
+                    girls.splice(left, 0, [ this.state.girl2 ]);
+                    const midGirl = this.getMidGirl();
+                    let nextGirlIdx = this.state.nextGirl;
+                    this.setState({
+                        sortedGirls: girls,
+                        progress: ((this.state.nextGirl - 1)/this.props.state.girls.length) * 100,
+                        pairCount: pairCount++,
+                        girl1: midGirl.girl,
+                        girl2: this.props.state.girls[nextGirlIdx],
+                        nextGirl: nextGirlIdx++,
+                        left: 0,
+                        right: girls.length
+                    });
+                } else {
+                    const midGirl = this.getMidGirl(left, right);
+                    this.setState({
+                        girl1: midGirl.girl,
+                        left: midGirl.idx
+                    });
+                }
                 break;
             case GIRL2:
+                // left = g1;
+                if (right - left === 1 || right - left === 0) {
+                    girls.splice(left, 0, [ this.state.girl2 ]);
+                    const midGirl = this.getMidGirl();
+                    let nextGirlIdx = this.state.nextGirl;
+                    this.setState({
+                        sortedGirls: girls,
+                        progress: ((this.state.nextGirl - 1)/this.props.state.girls.length) * 100,
+                        pairCount: pairCount++,
+                        girl1: midGirl.girl,
+                        girl2: this.props.state.girls[nextGirlIdx],
+                        nextGirl: nextGirlIdx++,
+                        left: 0,
+                        right: girls.length
+                    });
+                } else {
+                    const midGirl = this.getMidGirl(left, right);
+                    this.setState({
+                        girl1: midGirl.girl,
+                        right: midGirl.idx
+                    });
+                }
                 break;
             case TIED:
                 girls[g1].push(this.state.girl2);
@@ -67,8 +114,15 @@ export default class SortPicker extends Component {
         }
     }
 
-    insertGirl(idx) {
-        
+    getMidGirl(left = null, right = null) {
+        if (left === null && right === null) {
+            left = 0;
+            right = this.state.sortedGirls.length;
+        }
+
+        const midIdx = Math.floor((left + right) / 2);
+        const midGirl = { idx: midIdx, girl: this.state.sortedGirls[midIdx][0] };
+        return midGirl;
     }
 
     render() {
