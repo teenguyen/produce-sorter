434227368c7d4d7d171d79bbe3fb6555e498a081
RYUUSEiiSTAR <138484@RBA.GOV.AU>
2018-07-26 17:31:13 +1000

    I think the posCount might be wrong lool oops

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -13,31 +13,33 @@ export default function Result(props) {
     // top 3
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < girls[i].length; j++) {
-            top3.push(<MainIcon girl={girls[i][j]} rank={posCount} />);
+            top3.push(<MainIcon key={girls[i][j].name} girl={girls[i][j]} rank={posCount} />);
         }
         posCount++;
     }
 
     // top 12
-    for (let i = posCount; i < 13; i++) {
+    // because position is counted from 1, not 0~
+    for (let i = (posCount - 1); i < 12; i++) {
         for (let j = 0; j < girls[i].length; j++) {
-            top12.push(<MainIcon girl={girls[i][j]} rank={posCount} />);
+            top12.push(<MainIcon key={girls[i][j].name} girl={girls[i][j]} rank={posCount} />);
         }
         posCount++;
     }
 
-
     // all
-    for (let i = posCount; i < girls.length; i++) {
+    // because position is counted from 1, not 0~
+    for (let i = (posCount - 1); i < girls.length; i++) {
         for (let j = 0; j < girls[i].length; j++) {
             all.push(
-                <div>
+                <div className='flex' key={girls[i][j].name}>
                     <MiniIcon girl={girls[i][j]} rank={posCount} />
-                    {`${posCount}. ${girls[i][j].name}`}
+                    <div className='ranking'>{`${posCount}. `}</div>
+                    <p>{girls[i][j].name}</p>
                 </div>
             );
         }
-        posCount++
+        posCount++;
     }
 
     return(
