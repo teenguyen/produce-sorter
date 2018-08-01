dcdabafc95424c5708f031b5b637940f0867a0f3
RYUUSEiiSTAR <138484@RBA.GOV.AU>
2018-08-01 17:39:37 +1000

    Trying to clean up results page still

--- src/components/MainIcon.jsx
+++ src/components/MainIcon.jsx
@@ -8,12 +8,10 @@ export default function MainIcon(props) {
     
     return(
         <div className='main-icon'>
-            <div className='icon-image-desc'>
-                <img src={props.girl.img} alt={props.girl.name} />
-                <div className='desc'>
-                    <p>{ranking}{props.girl.name}</p>
-                    <p>{props.girl.company}</p>
-                </div>
+            <img src={props.girl.img} alt={props.girl.name} />
+            <div className='flex flex-center flex-wrap desc'>
+                <p>{ranking}{props.girl.name}</p>
+                <p>{props.girl.company}</p>
             </div>
         </div>
     );
--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -6,30 +6,40 @@ import MiniIcon from './../components/MiniIcon';
 export default function Result(props) {
     let posCount = 1;
     let girls = props.girls;
+    let top1 = [];
     let top3 = [];
     let top12 = [];
     let all = [];
 
     for (let i = 0; i < girls.length; i++) {
         let count = 0;
-        for (let j = 0; j < girls[i].length; j++) {
-            if (top3.length < 3) {
-                // let width = 100;
-                // if (i === 0) {
-                //     width = width/girls[0].length;
-                // } else {
-                //     let widthCount = girls[0].length;
-                //     while (widthCount < 3) {
-
-                //     }
-                // }
-
-                top3.push(<MainIcon key={girls[i][j].name} girl={girls[i][j]} rank={posCount} />);
+        if (i === 0) {
+            for (let j = 0; j < girls[i].length; j++) {
+                top1.push(
+                    <div className='top1-girl'>
+                        <MainIcon key={girls[i][j].name} girl={girls[i][j]} rank={posCount} />
+                    </div>);
                 count++;
-            } else if (top3.length + top12.length < 12) {
-                top12.push(<MainIcon key={girls[i][j].name} girl={girls[i][j]} rank={posCount} />);
+            }
+        }
+        else if (top1.length + top3.length < 3) {
+            for (let j = 0; j < girls[i].length; j++) {
+                top3.push(
+                    <div className='top3-girl'>
+                        <MainIcon key={girls[i][j].name} girl={girls[i][j]} rank={posCount} />
+                    </div>);
                 count++;
-            } else {
+            }
+        } else if (top1.length + top3.length + top12.length < 12) {
+            for (let j = 0; j < girls[i].length; j++) {
+                top12.push(
+                    <div className='top12-girl'>
+                        <MainIcon key={girls[i][j].name} girl={girls[i][j]} rank={posCount} />
+                    </div>);
+                count++;
+            }
+        } else {
+            for (let j = 0; j < girls[i].length; j++) {
                 all.push(
                     <div className='flex all-girl' key={girls[i][j].name}>
                         <MiniIcon girl={girls[i][j]} rank={posCount} />
@@ -48,6 +58,8 @@ export default function Result(props) {
 
     return(
         <div className='results'>
+            <div className='flex top1'>{top1}</div>
+            <br />
             <div className='flex top3'>{top3}</div>
             <br />
             <div className='flex top12'>{top12}</div>
