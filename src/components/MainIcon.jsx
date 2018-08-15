3807af24f0a1e196c07ab088544583777a1c7974
RYUUSEiiSTAR <138484@RBA.GOV.AU>
2018-08-15 15:50:01 +1000

    on smaller devices, names are aligned to wrapped text, and not underneath numbers

--- src/components/MainIcon.jsx
+++ src/components/MainIcon.jsx
@@ -3,15 +3,18 @@ import React from 'react';
 export default function MainIcon(props) {
     let ranking = null;
     if (typeof(props.rank) !== 'undefined') {
-        ranking = `${props.rank}. `;
+        ranking = <p>{`${props.rank}. `}</p>;
     }
     
     return(
         <div className='main-icon'>
             <img src={props.girl.img} alt={props.girl.name} />
             <div className='desc'>
-                <p>{ranking}{props.girl.name}</p>
-                <p>{props.girl.company}</p>
+                {ranking}
+                <div>
+                    <p>{props.girl.name}</p>
+                    <p>{props.girl.company}</p>
+                </div>
             </div>
         </div>
     );
