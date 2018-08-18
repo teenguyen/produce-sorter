7c0404a1532060ddec18868db591890276960f00
ryuuseiistar <tee.nguyen+github@live.com.au>
2018-08-18 14:18:08 +1000

    Prep for Elimination 3

--- src/pages/GroupPicker.jsx
+++ src/pages/GroupPicker.jsx
@@ -24,6 +24,8 @@ export default class GroupPicker extends Component {
     render() {
         let girlList = this.props.state.girls.filter(girl => {
             switch(this.state.currentGroup) {
+                // case '4':
+                //     return !girl.left && !girl.elim1 && !girl.elim2 && !girl.elim3;
                 case '3':
                     return !girl.left && !girl.elim1 && !girl.elim2;
                 case '2':
